042dc0a891512352e8f1082cc6b556b85855e510
Kanwal Arora <kanwalarora029@gmail.com>
2023-06-11 18:52:52 +0100

    Get PDF, TEXT and chumks function

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 from pyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def get_pdf_text(pdf_docs):
@@ -11,6 +14,23 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             pdf_text += page.extractText()        
     return pdf_text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        overlap_size=200,
+        length_function=len
+    )
+    text_chunks = text_splitter.split(text)
+    return text_chunks
+
+def get_vectorstores(chunks):
+    #embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings()
+    vectorstore = FAISS.from_texts(texts=chunks,embedding=embeddings)
+    return vectorstore
 
 
 def main():
@@ -32,8 +52,10 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
 
                 ## get text chunks
+                text_chunks = get_text_chunks(raw_text)
 
                 ## create vector store
+                vectorstore = get_vectorstores(text_chunks)
 
 if __name__=='__main__':
     main()
